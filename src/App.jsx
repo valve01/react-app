c10d5d26430f8d33298acafb7e3eb607c5876b7a
valve01 <118441831+valve01@users.noreply.github.com>
2023-06-15 12:38:56 +0300

    k

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 // Можно просто прописать путь до папки, без указания файла, но при условии, что файл будет называться index.jsx/index.js
 
 import './scss/app.scss';
@@ -27,14 +28,22 @@ function App() {
 							element={<Home />}
 						/> */}
 						<Route
+							path="/123"
+							element={<h1>123</h1>}
+						/>
+						<Route
+							path="/cart"
+							element={<Cart />}
+						/>
+						{/* <Route
 							path="/not-found"
 							element={<NotFound />}
-						/>
-{/* Конкрено в случае 404 страницы ее указывают для всех остальных путей, в конце перечня страниц */}
-						{/* <Route
+						/> */}
+						{/* Конкрено в случае 404 страницы ее указывают для всех остальных путей, в конце перечня страниц. Так Routes пройдется по всему перечню Route, при изменении адресной строки, и если найдет совпадения адресной строки и path - отобразит соответствующий контент. */}
+						<Route
 							path="*"
 							element={<NotFound />}
-						/> */}
+						/>
 						{/* Можно не просто вшивать значения, а использовать маски. В данном случае, если будут какие-то символы после pizzas/ то отрендерится указанный компонент */}
 						{/* <Route
 							path="/pizzas/:id"
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+	return (
+		<div>
+			<h1>Привет! это корзина</h1>
+		</div>
+	);
+};
+
+export default Cart;

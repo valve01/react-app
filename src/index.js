d12a6fefff273c28c42ec9a3fe78b35014de92ee
valve01 <118441831+valve01@users.noreply.github.com>
2023-06-15 12:47:16 +0300

    react-router-done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+	// Все наше приложение должно быть обернуто в <BrowserRouter></BrowserRouter>, так все приложение получает доступ к возможностям react-router и к его логике
 	<BrowserRouter>
 		<React.StrictMode>
 			<App />

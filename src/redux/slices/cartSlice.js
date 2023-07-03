8fcde7786aa611731047053e1328f224c7b76ce5
valve01 <118441831+valve01@users.noreply.github.com>
2023-07-03 12:11:15 +0300

    reduce

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,6 +10,10 @@ export const cartSlice = createSlice({
 		// Добавление в корзину в главном меню
 		addItem(state, action) {
 			state.items.push(action.payload);
+			// При добавлении товара сразу вычисляем общую стоимость товаров в корзине, используем для этого метод reduce
+			state.totalPrice = state.items.reduce((sum, obj) => {
+				return obj.price + sum;
+			}, 0);
 		},
 		// Удаление всех товаров данного типа из корзины(Крестик напротив элемента в корзине)
 		removeItem(state, action) {
@@ -18,10 +22,6 @@ export const cartSlice = createSlice({
 		// Полная очистка корзины
 		clearItems(state) {
 			state.items = [];
-		},
-		// Общая стоимость товаров в корзине
-		setTotalPrice(state, action) {
-			state.totalPrice = action.payload;
 		},
 	},
 });

b2fd81f1b201d31112228a0f1de4b9d670ffb756
valve01 <118441831+valve01@users.noreply.github.com>
2023-07-03 17:17:56 +0300

    addItem переделал, вопросы?

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -7,10 +7,26 @@ export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
+		// // Добавление в корзину в главном меню
+		// addItem(state, action) {
+		// 	state.items.push(action.payload);
+		// 	// При добавлении товара сразу вычисляем общую стоимость товаров в корзине, используем для этого метод reduce
+		// 	state.totalPrice = state.items.reduce((sum, obj) => {
+		// 		return sum + obj.price;
+		// 	}, 0);
+		// },
+
 		// Добавление в корзину в главном меню
 		addItem(state, action) {
-			state.items.push(action.payload);
-			// При добавлении товара сразу вычисляем общую стоимость товаров в корзине, используем для этого метод reduce
+			// Ищем элемент в массиве, и если находим добавляем ему к счетчику +1, иначе добавляем его в корзину и устанавливаем счетчик = 1
+
+			const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+			if (findItem) {
+				findItem.count++;
+			} else {
+				state.items.push({...action.payload, count: 1 });
+			}
 			state.totalPrice = state.items.reduce((sum, obj) => {
 				return sum + obj.price;
 			}, 0);

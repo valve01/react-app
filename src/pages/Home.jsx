b33eb2a02e45da72e576953bca5fb74428d27a4f
valve01 <118441831+valve01@users.noreply.github.com>
2023-07-14 13:07:02 +0300

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// ReduxToolkit - это JS библиотека, НЕ React библиотека => В ней нет хуков. Хуки берем из react-redux. Чтобы пользовать хуками из react их не нужно импортировать
 import { useSelector, useDispatch } from 'react-redux';
 
 // import qs from 'qs';
@@ -13,41 +12,21 @@ import { SearchContext } from '../App';
 import { setActiveCategory, setCurrentPage } from '../redux/slices/filterSlice';
 import { fetchPizzas } from '../redux/slices/pizzasSlice';
 const Home = () => {
-	// const [items, setItems] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
 
-	// const [activeSort, setActiveSort] = React.useState({
-	// 	name: 'популярности (сначала популярные)',
-	// 	sortProperty: 'rating',
-	// });
 	const { searchValue } = React.useContext(SearchContext);
-	// const [currentPage, setCurrentPage] = React.useState(1);
 
-	// Вытаскиваем activeCategory из Redux слайса. Этим хуком можно вообще все данные из хранилища вытащить (оно все записано в state. Получается наш state === store.reducer).
-	// В слайсах только логика хранится и начальное состояния стейтов, а значания стейтов лежат все в store. И достам мы их из store, обращаясь к его названиям слайсов и их свойствам, как в каталоге, а не напряму лезем в слайс
-	// const activeCategory = useSelector((state) => state.filter.activeCategory);
-	// Достаем функцию, которая будет изменение нашего стейта сохранять/записывать и помещать его в store. Как в мегафон будет кричать, чтобы state изменился
 	const dispatch = useDispatch();
-	// Функция, которую предоставляет нам хук useDispatch хранится также в store. store.dispatch===dispatch. Только store тогда вытащить нужно из store.js.
-	// dispatch достали и теперь он говорит, давай мне функцию, которая будет метять state. Ну мы и даем, она у нас в reducers прописана в слайсе - это setActiveCategory().
-	// На забываем только ее добсать из слайса
-	// import { setActiveCategory } from '../redux/slices/filterSlice';
-	// И в setActiveCategory() мы уже передаем index, который получили из Categories.jsx
+
 	const onClickSetActiveCategory = (index) => {
-		// Этот index мы передали ещё в Categories.jsx в onClick
-		// console.log(index);
 		dispatch(setActiveCategory(index));
-		// Теперь setActiveCategory(index) - вернет тот же объект типа, (что и console.log(action) в слайсе): {type: 'filters/setActiveCategory', payload: {index}}, а потом мы с помощью dispatch присваиваем значение ключа payload нашему стейту и сохраняем это изменение в store
 	};
-	// console.log('activeCategory:', activeCategory);
 
 	const onChangePage = (value) => {
 		dispatch(setCurrentPage(value));
 	};
 
-	// const activeSort = useSelector((state) => state.filter.sortType);
 	const { sortType, activeCategory, currentPage } = useSelector((state) => state.filter);
-	// const activeSort = sortType.sortProperty;
 
 	const items = useSelector((state) => state.pizzas.items);
 	const getPizzas = async () => {
@@ -59,18 +38,15 @@ const Home = () => {
 		const filter = searchValue ? searchValue : '';
 
 		try {
-
-			//setItems(res.data);
-
-			dispatch(fetchPizzas({
-				category,
-				sort,
-				order,
-				filter,
-				currentPage
-			}));
-
-			window.scrollTo(0, 0);
+			dispatch(
+				fetchPizzas({
+					category,
+					sort,
+					order,
+					filter,
+					currentPage,
+				}),
+			);
 		} catch (error) {
 			console.log('ERROR:', error);
 		} finally {
@@ -78,85 +54,19 @@ const Home = () => {
 		}
 	};
 
-	// console.log(activeSort);
 	const skeleton = [...new Array(4)].map((_, index) => <SkeletonPizzaBlock key={index} />);
 	const pizzas = items.map((obj) => {
 		return (
 			<PizzaBlock
 				key={obj.id}
-				// Так мы передаем в пропсы в PizzaBlock.jsx весь объект который преобразовали на очередной итерации массива методом map
 				{...obj}
 			/>
 		);
 	});
 
-	// Добавляем к стрелочной функции внутри useEffect ключевое слово async, чтобы иметь возможность пользоваться await
 	React.useEffect(() => {
-		// fetch(
-		// 	`https://64845cf9ee799e3216269459.mockapi.io/items?${
-		// 		activeCategory > 0 ? `category=${activeCategory}` : ''
-		// 	}&sortBy=${sortType.sortProperty.replace('-', '')}&order=${
-		// 		sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-		// 	}&filter=${searchValue ? searchValue : ''}&page=${currentPage}&limit=4`,
-		// )
-		// 	.then((res) => res.json())
-		// 	.then((arr) => {
-		// 		setItems(arr);
-		// 		setIsLoading(false);
-		// 	});
-		// Используя axios для запроса, нужно дописать сам метод get
-		// axios
-		// 	.get(
-		// 		`https://64845cf9ee799e3216269459.mockapi.io/items?${
-		// 			activeCategory > 0 ? `category=${activeCategory}` : ''
-		// 		}&sortBy=${sortType.sortProperty.replace('-', '')}&order=${
-		// 			sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-		// 		}&filter=${searchValue ? searchValue : ''}&page=${currentPage}&limit=4`,
-		// 	)
-		// 	// При использовании axios в response будут данные уже в js формате, а не в json, как при fetch, но они будут в виде объекта. Сама же основа будет храниться в свойстве data, обращаясь к нему мы пожемо пользоваться данными.
-		// 	.then((res) => {
-		// 		setItems(res.data);
-		// 		setIsLoading(false);
-		// 	})
-		// .catch((err)=>{
-		// console.log(err, "axios Error");
-		// // Говорим что загрузку нужно завершить в любом случае, даже при ошибке, иначе код прервет выполнение
-		// setIsLoading(false);
-		// });
-		// Чтобы при рендере автоматически страница вверх прокрутилась
-		// window.scrollTo(0, 0);
-		// // Переписываем запрос, используя await
-		// const fetchPizzas = async () => {
-		// 	setIsLoading(true);
-		// 	const res = await axios.get(
-		// 		`https://64845cf9ee799e3216269459.mockapi.io/items?${
-		// 			activeCategory > 0 ? `category=${activeCategory}` : ''
-		// 		}&sortBy=${sortType.sortProperty.replace('-', '')}&order=${
-		// 			sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-		// 		}&filter=${searchValue ? searchValue : ''}&page=${currentPage}&limit=4`,
-		// 	);
-		// 	// Больше не надо использовать then. Тело ответа от сервера мы просто записали в переменную res
-		// 	setItems(res.data);
-		// 	setIsLoading(false);
-		// 	// Чтобы при рендере автоматически страница вверх прокрутилась
-		// 	window.scrollTo(0, 0);
-		// };
-		// Переписываем запрос, используя отлов ошибок
-
-		getPizzas()
+		getPizzas();
 	}, [activeCategory, sortType, searchValue, currentPage]);
-
-	// React.useEffect =
-	// 	(() => {
-	// 		const queryString = qs.stringify({
-	// 			sortProperty: sortType.sortProperty,
-	// 			activeCategory: activeCategory,
-	// 			currentPage: currentPage,
-	// 		});
-	// 		console.log(queryString)
-	// 	},
-
-	// 	[activeCategory, sortType, searchValue, currentPage]);
 
 	return (
 		<>
@@ -164,14 +74,9 @@ const Home = () => {
 				<div className="content__top">
 					<Categories
 						activeCategory={activeCategory}
-						// 4 index - в стрелочной ф-ции ниже получен из
-						// Categories.jsx в строке onClick={() => {onClickSetActiveCategory(index)}}
 						onClickSetActiveCategory={onClickSetActiveCategory}
 					/>
-					<Sort
-					// activeSort={activeSort}
-					// onClickSetActiveSort={(listObj) => setActiveSort(listObj)}
-					/>
+					<Sort />
 				</div>
 				<h2 className="content__title">Все пиццы</h2>
 				<Pagination onChangePage={onChangePage} />

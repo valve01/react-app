3eb9594da424636312083fdd42fba1da7d2ce361
valve01 <118441831+valve01@users.noreply.github.com>
2023-08-02 12:38:00 +0300

    useLocation освоил

--- src/App.jsx
+++ src/App.jsx
@@ -6,98 +6,44 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
-
-// Можно просто прописать путь до папки, без указания файла, но при условии, что файл будет называться index.jsx/index.js
+import FullPizza from './components/FullPizza';
 
 import './scss/app.scss';
-
-// import pizzas from './assets/pizzas.json';
-
-// const SearchContext = React.createContext('');
-// Объект SearchContext содержит в себе компоненты Consumer и Provider
-// console.log(SearchContext)
 
 export const SearchContext = React.createContext();
 
 function App() {
-
-	// Прокинув useState по цепочке аж до search.jsx проверяем. Изменяя значение инпута в дочернем компоненте, в родителе перезаписывается значение переменной из хука useState.
-	// console.log(searchValue, 'input changed')
-
-	// Тут в state.counter.count - count - должен называться так же как и свойство в объекте в initialState в slice.js. В нашем случае это в filterSlice.js
-	// const initialState = {
-	// 	count: 0,
-	// }
-// А counter - должен называться также как свойство данного state в свойстве reducer в store.js
-
-
-	// const count = useSelector((state) => state.counter.count);
-	// const dispatch = useDispatch();
-
 	return (
 		<div className="wrapper">
-			
-			{/* <button
-				aria-label="Increment value"
-				onClick={() => dispatch(increment())}
-			>
-				Increment
-			</button>
-			<span>{count}</span>
-			<button
-				aria-label="Decrement value"
-				onClick={() => dispatch(decrement())}
-			>
-				Decrement
-			</button> */}
+			<Header />
+			<div className="content">
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
 
+					<Route
+						path="/123"
+						element={<h1>123</h1>}
+					/>
 
-				<Header
-				// Теперь не нужно прокидывать в Header переменные из useState, будем доставать их там из SearchContext. Комментим их
-				// searchValue={searchValue}
-				// setSearchValue={setSearchValue}
-				/>
-				<div className="content">
-					<Routes>
-						<Route
-							path="/"
-							element={
-								<Home
-								// searchValue={searchValue}
-								// setSearchValue={setSearchValue}
-								/>
-							}
-						/>
-						{/* / Для главной можно и не указывать в пути */}
-						{/* <Route
-							path=""
-							element={<Home />}
-						/> */}
-						<Route
-							path="/123"
-							element={<h1>123</h1>}
-						/>
-						<Route
-							path="/cart"
-							element={<Cart />}
-						/>
-						{/* <Route
-							path="/not-found"
-							element={<NotFound />}
-						/> */}
-						{/* Конкрено в случае 404 страницы ее указывают для всех остальных путей, в конце перечня страниц. Так Routes пройдется по всему перечню Route, при изменении адресной строки, и если найдет совпадения адресной строки и path - отобразит соответствующий контент. */}
-						<Route
-							path="*"
-							element={<NotFound />}
-						/>
-						{/* Можно не просто вшивать значения, а использовать маски. В данном случае, если будут какие-то символы после pizzas/ то отрендерится указанный компонент */}
-						{/* <Route
-							path="/pizzas/:id"
-							element={<NotFound />}
-						/> */}
-					</Routes>
-				</div>
+					<Route
+						path="/cart"
+						element={<Cart />}
+					/>
 
+					<Route
+						path="/pizza/:id"
+						element={<FullPizza />}
+					/>
+
+					<Route
+						path="*"
+						element={<NotFound />}
+					/>
+				</Routes>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/FullPizza.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FullPizza = () => {
+	return (
+		<div className='container'>
+			<img
+				src=""
+				alt=""
+			/>
+			<h2>Заголовок</h2>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, error deserunt veritatis quam
+				aspernatur non necessitatibus reiciendis impedit? Enim, quisquam ducimus. Obcaecati quia, fugit
+				facere cupiditate placeat magnam tenetur blanditiis.
+			</p>
+			<h4> 350 </h4>
+		</div>
+	);
+};
+
+export default FullPizza;

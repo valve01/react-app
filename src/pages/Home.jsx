542887cfb19e5f572d841f897d4cdaabf037b07e
valve01 <118441831+valve01@users.noreply.github.com>
2023-08-05 12:58:29 +0300

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // ReduxToolkit - это JS библиотека, НЕ React библиотека => В ней нет хуков. Хуки берем из react-redux. Чтобы пользовать хуками из react их не нужно импортировать
 import { useSelector, useDispatch } from 'react-redux';
 
-// import qs from 'qs';
+import qs from 'qs';
 import EmptyCart from '../components/EmptyCart';
 import PizzaBlock from '../components/PizzaBlock';
 import SkeletonPizzaBlock from '../components/PizzaBlock/Skeleton';
@@ -10,9 +10,11 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import Pagination from '../components/Pagination';
 
-import { setActiveCategory, setCurrentPage } from '../redux/slices/filterSlice';
+import { setActiveCategory, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 import { fetchPizzasFromRedux, selectorFilter, selectorPizzas } from '../redux/slices/pizzasSlice';
+import { useNavigate} from 'react-router-dom';
 const Home = () => {
+	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	const onClickSetActiveCategory = (index) => {
@@ -22,6 +24,13 @@ const Home = () => {
 	const onChangePage = (value) => {
 		dispatch(setCurrentPage(value));
 	};
+
+	React.useEffect(()=>{
+	const params =qs.parse(window.location.search.substring(1))
+	console.log(params)
+	dispatch(setFilters({...params}))
+	},[])
+
 
 	const { sortType, activeCategory, currentPage, searchValue } = useSelector(selectorFilter);
 
@@ -49,15 +58,18 @@ const Home = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [activeCategory, sortType, searchValue, currentPage]);
 
+	React.useEffect(() => {
+		const queryString = qs.stringify({
+			sortProperty: sortType.sortProperty,
+			activeCategory: activeCategory,
+			currentPage: currentPage,
+		});
+		console.log(queryString);
+		navigate(`?${queryString}`);
+	}, [activeCategory, sortType, searchValue, currentPage]);
+
 	const skeleton = [...new Array(4)].map((_, index) => <SkeletonPizzaBlock key={index} />);
-	// const pizzas = items.map((obj) => (
-	// 	<Link
-	// 		key={obj.id}
-	// 		to={`/pizza/${obj.id}`}
-	// 	>
-	// 		<PizzaBlock {...obj} />
-	// 	</Link>
-	// ));
+
 	const pizzas = items.map((obj) => (
 		<PizzaBlock
 			key={obj.id}
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -3,7 +3,7 @@ const initialState = {
 	sortType: { name: 'популярности (сначала популярные)', sortProperty: 'rating' },
 	activeCategory: 0,
 	currentPage: 1,
-	searchValue:''
+	searchValue: '',
 };
 export const filterSlice = createSlice({
 	name: 'filters',
@@ -25,11 +25,17 @@ export const filterSlice = createSlice({
 		setSearchValue(state, action) {
 			state.searchValue = action.payload;
 		},
+		setFilters(state, action) {
+			state.currentPage = Number(action.payload.currentPage) ;
+			state.activeCategory = Number(action.payload.activeCategory) ;
+			state.sortType= action.payload.sortType;
+		},
 	},
 });
 
-export const selectorSort = (state) => state.filter.sortType
+export const selectorSort = (state) => state.filter.sortType;
 
-export const { setActiveCategory, setActiveSortType, setCurrentPage, setSearchValue } = filterSlice.actions;
+export const { setActiveCategory, setActiveSortType, setCurrentPage, setSearchValue, setFilters } =
+	filterSlice.actions;
 
 export default filterSlice.reducer;

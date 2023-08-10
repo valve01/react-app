867999ed9b95e4979170e29bcc2fc54166d5cd3f
valve01 <118441831+valve01@users.noreply.github.com>
2023-08-10 09:28:15 +0300

    в

--- src/components/FullPizza.tsx
+++ src/components/FullPizza.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const FullPizza: React.FC = () => {
-	const [pizza, setPizza] = React.useState();
+	const [pizza, setPizza] = React.useState<{
+		imageUrl: string;
+		title: string;
+		price: number;
+	}>();
 	// const params = useParams();
 	// console.log(params);
 	const { id } = useParams();
@@ -22,8 +26,31 @@ const FullPizza: React.FC = () => {
 		fetchPizza();
 		// eslint-disable-next-line
 	}, [id]);
-
+	// До проверки pizza - это объект или undefined
+// const pizza: {
+//     imageUrl: string;
+//     title: string;
+//     price: number;
+// } | undefined
 	if (pizza) {
+// Благодаря проверке выше последующий код уверен, что pizza не будет undefined 
+// Поэтому при наведении на pizza больше не показывает что она может быть undefined
+// 		const pizza: {
+//     imageUrl: string;
+//     title: string;
+//     price: number;
+// }
+// Такой же эффект будет если передать объкет по умолчанию в () во время использования useState
+	// const [pizza, setPizza] = React.useState<{
+	// 	imageUrl: string;
+	// 	title: string;
+	// 	price: number;
+	//	}>({
+	// 	imageUrl: "";
+	// 	title: "";
+	// 	price: 0;
+	// 	})
+	//	Тогда такая проверка не понадобится
 		return (
 			<div className="container">
 				<img

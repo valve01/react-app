da62423bc074cd41101934e0ff95b6b29d2a019b
valve01 <118441831+valve01@users.noreply.github.com>
2023-09-20 12:42:04 +0300

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './components/FullPizza';
+// import NotFound from './pages/NotFound';
+// import Cart from './pages/Cart';
+// import FullPizza from './components/FullPizza';
 import MainLayout from './layouts/MainLayout';
 import './scss/app.scss';
 
-
+const Cart = React.lazy(() => import('./pages/Cart'));
+const FullPizza = React.lazy(() => import('./components/FullPizza'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 function App() {
 	return (
+
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
 				<Route path="" element={<Home />} />
-				<Route path="/123" element={<h1>123</h1>} />
-				<Route path="/cart" element={<Cart />} />
+				<Route path="/cart" element={ 
+				<Suspense fallback={<div>Loading...</div>}> 
+				<Cart /> 
+				</Suspense> } />
 				<Route path="/pizza/:id" element={<FullPizza />} />
 				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
+
 	);
 }
 

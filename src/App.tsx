895415e3907f1a29c53a80b087f0aa28fa636b4d
valve01 <118441831+valve01@users.noreply.github.com>
2023-10-09 13:23:51 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 
 
 import Home from './pages/Home';
+
 // import NotFound from './pages/NotFound';
 // import Cart from './pages/Cart';
 // import FullPizza from './components/FullPizza';
+
 import MainLayout from './layouts/MainLayout';
 import './scss/app.scss';
 
@@ -14,19 +16,22 @@ const FullPizza = React.lazy(() => import('./components/FullPizza'));
 const NotFound = React.lazy(() => import('./pages/NotFound'));
 function App() {
 	return (
-
+		<Suspense fallback={<div>Loading...</div>}> 
 		<Routes>
 			<Route path="/" element={<MainLayout />}>
+
 				<Route path="" element={<Home />} />
-				<Route path="/cart" element={ 
-				<Suspense fallback={<div>Loading...</div>}> 
-				<Cart /> 
-				</Suspense> } />
+
+
+				<Route path="/cart" element={<Cart />} />
 				<Route path="/pizza/:id" element={<FullPizza />} />
+
+
 				<Route path="*" element={<NotFound />} />
+
 			</Route>
 		</Routes>
-
+		</Suspense>  
 	);
 }
 

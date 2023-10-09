86db23f6c475e2fdbfb6482f0fca0a5cf37c80aa
valve01 <118441831+valve01@users.noreply.github.com>
2023-10-09 13:34:07 +0300

    last working. w/o lazy (it's crushing the project)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Loadable from 'react-loadable';
+// import Loadable from 'react-loadable';
 
 import Home from './pages/Home';
-// import NotFound from './pages/NotFound';
-// import Cart from './pages/Cart';
-// import FullPizza from './components/FullPizza';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+import FullPizza from './components/FullPizza';
 import MainLayout from './layouts/MainLayout';
 import './scss/app.scss';
 
 // const Cart = React.lazy(() => import(/*webpackChunkName: "Cart"*/'./pages/Cart'));
 
-const Cart = Loadable({
-	loader: () => import(/*webpackChunkName: "Cart"*/ './pages/Cart'),
-	loading: () => <div>Загрузка</div>,
-});
+// const Cart = Loadable({
+// 	loader: () => import(/*webpackChunkName: "Cart"*/ './pages/Cart'),
+// 	loading: () => <div>Загрузка</div>,
+// });
 
-const FullPizza = React.lazy(() => import(/*webpackChunkName: "FullPizza"*/ './components/FullPizza'));
+// const FullPizza = React.lazy(() => import(/*webpackChunkName: "FullPizza"*/ './components/FullPizza'));
 
-// const FullPizzaPage="FullPizza"
-// const FullPizza = React.lazy(() => import(/*webpackChunkName: "[request]"*/`./components/${FullPizzaPage}`));
+// // const FullPizzaPage="FullPizza"
+// // const FullPizza = React.lazy(() => import(/*webpackChunkName: "[request]"*/`./components/${FullPizzaPage}`));
 
-const NotFound = React.lazy(() => import(/*webpackChunkName: "NotFound"*/ './pages/NotFound'));
+// const NotFound = React.lazy(() => import(/*webpackChunkName: "NotFound"*/ './pages/NotFound'));
 function App() {
 	return (
 		<Routes>
